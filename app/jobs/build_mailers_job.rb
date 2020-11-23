f4f32c16fffed5467fb0fd473c820a396f725cec
Andrei Andreev <andrey.b0nn1e@gmail.com>
2020-11-23 19:43:21 +0300

    Add sender service

--- app/jobs/build_mailers_job.rb
+++ app/jobs/build_mailers_job.rb
@@ -2,13 +2,6 @@ class BuildMailersJob < ApplicationJob
   queue_as :default
 
   def perform(campaign_id:)
-    campaign = Campaign.joins(:customers).find(campaign_id)
-    campaign.customers.each do |customer|
-      SendMailJob.perform_later(
-        email: customer.email,
-        subject: campaign.subject,
-        message: campaign.message
-      )
-    end
+    Campaigns::Sender.call(campaign_id: campaign_id)
   end
 end
---
+++ spec/services/campaign/sender_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+describe Campaigns::Sender do
+  let(:campaign) { create(:campaign, customers: [customer1, customer2]) }
+  let(:customer1) { create(:customer) }
+  let(:customer2) { create(:customer) }
+  let(:args1) do
+    {
+      email: customer1.email,
+      subject: campaign.subject,
+      message: campaign.message
+    }
+  end
+  let(:args2) do
+    {
+      email: customer2.email,
+      subject: campaign.subject,
+      message: campaign.message
+    }
+  end
+
+  before do
+    allow(SendMailJob).to receive(:perform_later)
+    described_class.call(campaign_id: campaign.id)
+  end
+
+  it { expect(SendMailJob).to have_received(:perform_later).with(args1) }
+  it { expect(SendMailJob).to have_received(:perform_later).with(args2) }
+  it { expect(SendMailJob).to have_received(:perform_later).twice }
+end
