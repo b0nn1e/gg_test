0c75dc4c31d20e8a7efc6be458bd979547a66f2f
Andrei Andreev <andrey.b0nn1e@gmail.com>
2020-11-23 19:43:21 +0300

    Add sender service

---
+++ app/services/campaigns/sender.rb
@@ -0,0 +1,22 @@
+module Campaigns
+  class Sender < ApplicationService
+    attr_accessor :campaign_id
+
+    def initialize(campaign_id:)
+      super
+
+      self.campaign_id = campaign_id
+    end
+
+    def call
+      campaign = Campaign.joins(:customers).find(campaign_id)
+      campaign.customers.each do |customer|
+        SendMailJob.perform_later(
+          email: customer.email,
+          subject: campaign.subject,
+          message: campaign.message
+        )
+      end
+    end
+  end
+end
